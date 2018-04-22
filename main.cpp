ae8f785a040adc92c8fcb481bf92a1e26b67b677
wantedHorizon <33939097+wantedHorizon@users.noreply.github.com>
2018-04-22 19:40:58 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
-#include "Member.h"
 using namespace std;
 
-Member avi,chana,beni;
+#include "CircularInt.hpp"
 
-
-void test1() {
-    Member dana;
-    chana.follow(dana);
-    dana.follow(avi);
-    cout << "  " << chana.numFollowers() << " " <<  chana.numFollowing() << endl; // 0 1
-    cout << "  " << avi.numFollowers() << " " <<  avi.numFollowing() << endl; // 1 0
-    cout << "  " << Member::count() << endl; // 4
-}
 
 int main() {
+	CircularInt hour {1, 12};                 // <hour is an integer between 1 and 12, like an hour on the clock>
+	cout << hour << endl;                     // 1
+	hour += 4;  cout << hour << endl;         // 5
+	(hour += 2)++;  cout << hour << endl;     // 8
+	hour += 18;   cout << hour << endl;       // 2   (18 hours after 8)
+	cout << -hour << endl;                    // 10  (2 hours before midnight)
+	hour = 1 - hour; cout << hour << endl;    // 11  (2 hours before 1)
+	cout << hour+hour << endl;                // 10 (11 hours after 11)
+	hour *= 2;   cout << hour << endl;        // 10 (11*2 = 11+11)
+	cout << hour/2 << endl;                   // TWO OPTIONS: 11 (since 11*2=10) or 5 (since 5*2=10 too).
 
-    cout << avi.numFollowers() << " " << avi.numFollowing() << endl; // 0 0
-    avi.follow(beni);
-    cout << avi.numFollowers() << " " << avi.numFollowing() << endl; // 0 1
-    cout << beni.numFollowers() << " " << beni.numFollowing() << endl; // 1 0
-    cout << Member::count() << endl; // 3
-    cout << endl;
+	try {
+		cout << hour/3;
+	} catch (const string& message) {
+		cout << message << endl;     // "There is no number x in {1,12} such that x*3=10"
+	}
 
-    avi.follow(beni); // duplicate follow has no effect
-    cout << avi.numFollowers() << " " << avi.numFollowing() << endl; // 0 1
-    avi.unfollow(beni);
-    cout << avi.numFollowers() << " " << avi.numFollowing() << endl; // 0 0
-    cout << endl;
-
-    cout << chana.numFollowers() << " " <<  chana.numFollowing() << endl; // 0 0
-  test1();
-    cout <<"chana "<< chana.numFollowers() << " " <<  chana.numFollowing() << endl; // 0 0
-    cout <<"avi "<< avi.numFollowers() << " " <<  avi.numFollowing() << endl; // 0 0
-    cout << Member::count() << endl; // 3
+	// RIDDLES (not for submission): 
+	//  * when is there exactly one answer to a/b?
+	//  * when are there two or more answers to a/b?
+	//	* when is there no answer to a/b?
 }
